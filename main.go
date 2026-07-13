4bf5808a79f80a5fa17fbcc6493849e3195b8d9c
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-13 11:12:07 +0500

    Added index.html to the project

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "net/http"
 
 func main() {
 	ServeMux := http.NewServeMux()
+	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
 	MyServer := http.Server{}
 	MyServer.Handler = ServeMux
 	MyServer.Addr = ":8080"
