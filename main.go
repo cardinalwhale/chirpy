5713204b7d551f9c6f5c52f108136c86c303a00b
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-13 11:38:59 +0500

    Added custom handler

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "net/http"
 
 func main() {
 	ServeMux := http.NewServeMux()
-	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	ServeMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
 	ServeMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	ServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 	MyServer := http.Server{}
 	MyServer.Handler = ServeMux
 	MyServer.Addr = ":8080"
