0e0957c4ffce600f947c0ddc9ba56031b8505007
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-13 11:23:02 +0500

    Added a picture to show

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "net/http"
 func main() {
 	ServeMux := http.NewServeMux()
 	ServeMux.Handle("/", http.FileServer(http.Dir(".")))
+	ServeMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
 	MyServer := http.Server{}
 	MyServer.Handler = ServeMux
 	MyServer.Addr = ":8080"
