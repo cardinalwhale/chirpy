2ea9a1e67f82b3e5b6938659dde50d0d357bc173
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-14 15:52:11 +0500

    Additional handling for GET and POST requests

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
+	apiCfg := apiConfig{}
 	ServeMux := http.NewServeMux()
-	ServeMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	ServeMux.Handle("/app", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	ServeMux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
-	ServeMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	ServeMux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
+	})
+	ServeMux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "Hits: %d", apiCfg.fileServerHits.Load())
+	})
+	ServeMux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCfg.fileServerHits.Swap(0)
 	})
 	MyServer := http.Server{}
 	MyServer.Handler = ServeMux
