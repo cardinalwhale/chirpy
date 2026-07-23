6269a9c5d31cfc7246e0d166bae5ef687bb54492
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-23 16:36:30 +0500

    Some small fixes in formatting

--- handler_chirps_create.go
+++ handler_chirps_create.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Chirp struct {
-	ID        uuid.UUID `json:"id`
+	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Body      string    `json:"body"`
