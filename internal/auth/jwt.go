56a59392ec4a572ca1c6e46d858bc15c5c40e6f5
Nikita Nikolayev <cardinalwhale@tutanota.com>
2026-07-24 14:34:31 +0500

    Some work done on JWT authorization

---
+++ internal/auth/jwt.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	jwt.NewWithClaims(jwt.SigningMethodES256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	return "bruh", nil
+}
